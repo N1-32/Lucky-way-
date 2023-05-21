03c14aa17d35913c813cf9c413a14ebc86be901b
Vasylyna Venhryn <venhryn15@gmail.com>
2023-05-22 00:33:23 +0300

    fixed bugs

--- code.js
+++ code.js
@@ -1,189 +1,148 @@
+// функція відображення ресторанів
+function drawRestaurants(restaurants)
+{
+	var mainContainer = document.querySelector('.container');
+    	
+    // очищення головного контейнеру
+    mainContainer.innerHTML = "";
 
-for(let i=0; i<restaurants.length; i++){
-  
-    
-	var mainContaner = document.querySelector('.container');
+	for (let i = 0; i < restaurants.length; i++) {
+		let dis = 0;
+		let s = "m";
+		if (restaurants[i].dst !== undefined) {
+			dis = restaurants[i].dst;
+			if (dis >= 1000) {
+				s = "km";
+				dis = dis / 1000;
+			}
+
+			console.log(restaurants[i].id, dis, s);
+        }
+		
+		var container = document.createElement('div');
+		container.classList.add('wrapper'); 
    
-	var container = document.createElement('div');
-	container.classList.add('wrapper'); 
+		var card = document.createElement('div');
+		card.classList.add('card');
    
-	var card = document.createElement('div');
-	card.classList.add('card');
-   
-	var imgSrc = document.createElement('img');
-	imgSrc.src = restaurants[i].imgSrc;
-	imgSrc.classList.add('card-img');
+		var imgSrc = document.createElement('img');
+		imgSrc.src = restaurants[i].imgSrc;
+		imgSrc.classList.add('card-img');
 	
-	card.appendChild(imgSrc);
+		card.appendChild(imgSrc);
    
-	var nam = document.createElement('div');
-	nam.classList.add('card-title');
-	nam.textContent = restaurants[i].name;
-	card.appendChild(nam);
-   //новий блок
-	var cardsub = document.createElement('div');
-	cardsub.classList.add('card-sub');
+		var nam = document.createElement('div');
+		nam.classList.add('card-title'	);
+		nam.textContent = restaurants[i].name;
+		card.appendChild(nam);
+		//новий блок
+		var cardsub = document.createElement('div');
+		cardsub.classList.add('card-sub');
    
-   //додавання лінії
-   var line1 = document.createElement('img');
-   line1.classList.add('line');
-   line1.src = 'Line 56.svg';
-   cardsub.appendChild(line1);
-   //адреса
-	var location = document.createElement('div');
-   location.classList.add('location');
-   location.textContent = restaurants[i].location;
-   cardsub.appendChild(location);
-   //додавання лінії
-   
-   var line2 = document.createElement('img');
-   line2.classList.add('line');
-   line2.src ='Line 56.svg';
-   cardsub.appendChild(line2);
-   card.appendChild(cardsub);
-   //додавання кнопки в блок card
-   var butt = document.createElement('a');
-   butt.href = "меню/menu.html";
-   butt.classList.add('menu_btn');
-   butt.textContent = 'Перейти';
-   card.appendChild(butt);
-   //додавання блоку кард в блок container
-	container.appendChild(card);
-   
-	mainContaner.appendChild(container);
-   }
-   
-   function getGeooloc(){
-   let d1 = document.querySelectorAll('.wrapper')
-	
-   
-   var unit = 'metres';
-   
-   
-   function getDistanceBetweenPoints(lat1, lon1, lat2, lon2, unit = 'miles') {
-	   let theta = lon1 - lon2;
-	   let distance = 60 * 1.1515 * (180/Math.PI) * Math.acos(
-		   Math.sin(lat1 * (Math.PI/180)) * Math.sin(lat2 * (Math.PI/180)) + 
-		   Math.cos(lat1 * (Math.PI/180)) * Math.cos(lat2 * (Math.PI/180)) * Math.cos(theta * (Math.PI/180))
-	   );
-	   if (unit == 'miles') {
-		   return Math.round(distance, 2);
-	   } else if (unit == 'metres') {
-		   return Math.round(distance * 1609.344, 2);
-	   }
-   }
-	 
-   let t = document.getElementById('p');
-   let distances = [];
-	   if(navigator.geolocation ){
-			   navigator.geolocation.getCurrentPosition(Position); }
-			   else{
-				 t = documet.getElementById('p').innerHTML = " error ";
-			   }
-   function compareByDST(a, b) {
-	   if (a.dst < b.dst) {
-		 return -1;
-	   }
-	   if (a.dst > b.dst) {
-		 return 1;
-	   }
-	   return 0;
-	 }
-   
-   function Position(positon){
-	   d1.forEach(function(d1) {
-		   d1.remove();
-		 });
-	   let lon2= positon.coords.longitude;
-	   let lat2 = positon.coords.latitude;
-	   for(let i=0 ; i<json.length; i++){
-		   let lat1 = json[i].location.latitude;
-		   let lon1 = json[i].location.longitude;
-		   let distance = getDistanceBetweenPoints(lat1, lon1, lat2, lon2, unit);
-		   
-		   //json.sory(sortarr);
-		   console.log(`${json[i].id} ${distance} м`);
-	   
-	   
-		  distances.push({
-		   id: json[i].id,
-		   dst : distance,
-		   name : json[i].name,
-		   category_id : json[i].category_id,
-		   imgSrc: json[i].imgSrc,
-		   address : json[i].address,
-		   location :{ latitude: json[i].latitude ,
-			   longitude: json[i].longitude },
-		   chedule :json[i].chedule,
-		   website : json[i].website,
-	   });
-   }
-		   
-		   distances.sort(compareByDST);
-		   var s ;
-		   
-	   for(let i=0; i<distances.length; i++){
-		   dis = distances[i].dst;
-		   if (dis < 1000) {
-			   s = "m";
-			  console.log(dis,s);
-			} else {
-			   s = "km";
-			dis = dis / 1000;}
-			console.log(distances[i].id, dis, s);
-   
-			var mainContaner = document.querySelector('.container');
-   
-			var container = document.createElement('div');
-			container.classList.add('wrapper');
-		   
-			var card = document.createElement('div');
-			card.classList.add('card');
-		   
-			var imgSrc = document.createElement('img');
-			imgSrc.src = distances[i].imgSrc;
-			imgSrc.classList.add('card-img');
-			
-			card.appendChild(imgSrc);
-		   
-			var nam = document.createElement('div');
-			nam.classList.add('card-title');
-			nam.textContent = distances[i].name;
-			card.appendChild(nam);
-		   //новий блок
-			var cardsub = document.createElement('div');
-			cardsub.classList.add('card-sub');
-		   
-		   //додавання лінії
-		   var line1 = document.createElement('img');
-		   line1.classList.add('line');
-		   line1.src = 'Line 56.svg';
-		   cardsub.appendChild(line1);
-		   //адреса
+		//додавання ліні	ї
+		var line1 = document.createElement('img');
+		line1.classList.add('line')	;
+		line1.src = 'Line 56.svg'	;
+		cardsub.appendChild(line1)	;
+		//адреса
+		var address = document.createElement('div');
+		address.classList.add('location');
+		address.textContent = restaurants[i].address;
+		cardsub.appendChild(address);
+
+		// дистанція
+		if (restaurants[i].dst !== undefined) {
 			var location = document.createElement('div');
-		   location.classList.add('location');
-		   location.textContent = distances[i].location + dis + s;
-		   cardsub.appendChild(location);
-		   //додавання лінії
-		   
-		   var line2 = document.createElement('img');
-		   line2.classList.add('line');
-		   line2.src = 'Line 56.svg';
-		   cardsub.appendChild(line2);
-		   card.appendChild(cardsub);
-		   //додавання кнопки в блок card
-		   var butt = document.createElement('a');
-		   butt.href = "меню/menu.html";;
-		   butt.classList.add('menu_btn');
-		   butt.textContent = 'Перейти';
-		   card.appendChild(butt);
-		   //додавання блоку кард в блок container
-			container.appendChild(card);
-		   
-			mainContaner.appendChild(container);
-		   
-	   
-   }
-   }
+			location.classList.add('location');
+			location.textContent = dis + s;
+			cardsub.appendChild(location);
+        }
+		
+		//додавання лінії
    
-   }
+		var line2 = document.createElement('img');
+		line2.classList.add('line');
+		line2.src ='Line 56.svg';
+		cardsub.appendChild(line2);
+		card.appendChild(cardsub);
+		//додавання кнопки в блок card
+		var butt = document.createElement('a');
+		butt.href = "меню/menu.html";
+		butt.classList.add('menu_btn');
+		butt.textContent = 'Перейти';
+		card.appendChild(butt);
+		//додавання блоку кард в блок container
+		container.appendChild(card);
+   
+		mainContainer.appendChild(container);
+	}
+}
+function getGeooloc(restaurants){	
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function (position) { Position(position, restaurants); });
+	}
+	else {
+		let t = document.getElementById('p');
+		t.innerHTML = " error ";
+	}
+}
+
+function getDistanceBetweenPoints(lat1, lon1, lat2, lon2, unit = 'miles') {
+	let theta = lon1 - lon2;
+	let distance = 60 * 1.1515 * (180 / Math.PI) * Math.acos(
+		Math.sin(lat1 * (Math.PI / 180)) * Math.sin(lat2 * (Math.PI / 180)) +
+		Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) * Math.cos(theta * (Math.PI / 180))
+	);
+	if (unit == 'miles') {
+		return Math.round(distance, 2);
+	} else if (unit == 'metres') {
+		return Math.round(distance * 1609.344, 2);
+	}
+}
+
+function compareByDST(a, b) {
+	if (a.dst < b.dst) {
+		return -1;
+	}
+	if (a.dst > b.dst) {
+		return 1;
+	}
+	return 0;
+}
+
+function Position(positon, restaurants) {
+	var unit = 'metres';
+	let d1 = document.querySelectorAll('.wrapper');  
+	let distances = [];
+
+	d1.forEach(function (d1) {
+		d1.remove();
+	});
+	let lon2 = positon.coords.longitude;
+	let lat2 = positon.coords.latitude;
+	for (let i = 0; i < restaurants.length; i++) {
+		let lat1 = restaurants[i].location.latitude;
+		let lon1 = restaurants[i].location.longitude;
+		let distance = getDistanceBetweenPoints(lat1, lon1, lat2, lon2, unit);
+
+		//json.sory(sortarr);
+		console.log(`${restaurants[i].id} ${distance} м`);
+
+		distances.push({
+			id: restaurants[i].id,
+			dst: distance,
+			name: restaurants[i].name,
+			category_id: restaurants[i].category_id,
+			imgSrc: restaurants[i].imgSrc,
+			address: restaurants[i].address,
+			location: {
+				latitude: restaurants[i].latitude,
+				longitude: restaurants[i].longitude
+			},
+			chedule: restaurants[i].chedule,
+			website: restaurants[i].website,
+		});
+	}
+	distances.sort(compareByDST);
+	drawRestaurants(distances);
+}
